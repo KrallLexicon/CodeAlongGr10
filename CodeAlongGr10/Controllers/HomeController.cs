e2aec6f4292bd409599ca702eec723498542b30e
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2022-11-09 10:42:04 +0100

    CodeAlong ViewModels, Partial Views, Ajax

--- CodeAlongGr10/Controllers/HomeController.cs
+++ CodeAlongGr10/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeAlongGr10.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodeAlongGr10.Controllers
 {
     public class HomeController : Controller
     {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
         public IActionResult Index()
         {
-            return View();
+            if (PersonViewModel.listOfPeople.Count == 0)
+                PersonViewModel.GeneratePeople();
+
+            PersonViewModel vm = new PersonViewModel();
+
+            vm.tempList = PersonViewModel.listOfPeople;
+
+            return View(vm);
         }
         public IActionResult About()
         {
---
+++ CodeAlongGr10/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeAlongGr10.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
