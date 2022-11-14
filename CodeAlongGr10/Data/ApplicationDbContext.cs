2872fd6f42b741cf4f998a8e4835488e34deb272
Jonathan Krall Olsson <jonathan_olsson88@hotmail.com>
2022-11-14 10:30:29 +0100

    CodeAlong to get started with EF

---
+++ CodeAlongGr10/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeAlongGr10.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeAlongGr10.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
--- CodeAlongGr10/Models/Person.cs
+++ CodeAlongGr10/Models/Person.cs
@@ -1,13 +1,16 @@
-﻿namespace CodeAlongGr10.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeAlongGr10.Models
 {
     public class Person
     {
+        [Key]
         public string Id { get; set; }
 
         public string Name { get; set; }
 
         public int Age { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
